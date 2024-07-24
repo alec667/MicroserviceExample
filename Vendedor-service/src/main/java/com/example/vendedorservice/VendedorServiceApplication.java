ff8ef671193251ecc9d85d70104c8cbe268aa6bf
Alejandro Casiva <alec667@gmail.com>
2024-07-24 20:58:10 -0300

    Vendedor-service First commit

---
+++ Vendedor-service/src/main/java/com/example/vendedorservice/VendedorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.vendedorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendedorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendedorServiceApplication.class, args);
+    }
+
+}
