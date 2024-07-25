7d133faa52622f1056addf678b53de8b7fe67692
Alejandro Casiva <alec667@gmail.com>
2024-07-24 21:43:14 -0300

    feature(Vendedor-service/service.VendedorServiceImpl): add VendedorServiceImpl class

---
+++ Vendedor-service/src/main/java/com/example/vendedorservice/service/impl/VendedorServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.vendedorservice.service.impl;
+
+import com.example.vendedorservice.model.Vendedor;
+import com.example.vendedorservice.service.VendedorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VendedorServiceImpl implements VendedorService {
+    @Override
+    public Vendedor getVendedor(Integer vendedorId) {
+        return null;
+    }
+
+    @Override
+    public List<Vendedor> getAllVendedor() {
+        return null;
+    }
+
+    @Override
+    public String createVendedor(Vendedor vendedor) {
+        return null;
+    }
+
+    @Override
+    public Vendedor updateVendedor(Vendedor vendedor) {
+        return null;
+    }
+
+    @Override
+    public String deleteVendedor(Integer vendedorId) {
+        return null;
+    }
+}
