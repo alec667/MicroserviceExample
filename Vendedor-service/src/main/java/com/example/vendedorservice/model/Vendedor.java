00c8c2f75b58dcb6c1b3c3cc423895d99eb711f2
Alejandro Casiva <alec667@gmail.com>
2024-07-24 21:18:49 -0300

    feature(Vendedor-service/model.Vendedor): add Vendedor class

---
+++ Vendedor-service/src/main/java/com/example/vendedorservice/model/Vendedor.java
@@ -0,0 +1,10 @@
+package com.example.vendedorservice.model;
+
+import lombok.Data;
+
+@Data
+public class Vendedor {
+    private Integer vendedorId;
+    private String VendedorName;
+    private String VendedorPhone;
+}
