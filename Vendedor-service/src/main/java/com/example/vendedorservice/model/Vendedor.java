373a6952d65a327fe301b42b29aac862f9770051
Alejandro Casiva <alec667@gmail.com>
2024-07-24 21:54:43 -0300

    refactor(Vendedor-service/model.Vendedor): add entity and id annotation

--- Vendedor-service/src/main/java/com/example/vendedorservice/model/Vendedor.java
+++ Vendedor-service/src/main/java/com/example/vendedorservice/model/Vendedor.java
@@ -1,9 +1,14 @@
 package com.example.vendedorservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Vendedor {
+
+    @Id
     private Integer vendedorId;
     private String VendedorName;
     private String VendedorPhone;
