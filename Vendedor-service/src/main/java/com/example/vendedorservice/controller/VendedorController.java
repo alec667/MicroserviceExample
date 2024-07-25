8e93d55cd4d0c343b494287a5e19f72fc9cd0c7c
Alejandro Casiva <alec667@gmail.com>
2024-07-24 21:52:37 -0300

    feature(Vendedor-service/controller.VendedorController): add create method

--- Vendedor-service/src/main/java/com/example/vendedorservice/controller/VendedorController.java
+++ Vendedor-service/src/main/java/com/example/vendedorservice/controller/VendedorController.java
@@ -2,6 +2,9 @@ package com.example.vendedorservice.controller;
 
 
 import com.example.vendedorservice.model.Vendedor;
+import com.example.vendedorservice.service.VendedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "vendedor")
 public class VendedorController {
 
+    @Autowired
+    VendedorService vendedorService;
+
     @PostMapping(path = "create", consumes = "application/json")
     public ResponseEntity<String> createVendedor(@RequestBody Vendedor vendedor){
-        return null;
+        return new ResponseEntity<>(vendedorService.createVendedor(vendedor), HttpStatus.CREATED);
     }
 
 }
