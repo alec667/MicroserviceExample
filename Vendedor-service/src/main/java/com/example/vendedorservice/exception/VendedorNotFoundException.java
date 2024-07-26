68ce7a8b7f40b8f7976107d8a5cce617e04cbe89
Alejandro Casiva <alec667@gmail.com>
2024-07-25 23:15:55 -0300

    feature(Vendedor-service/exception.VendedorNotFoundException): add VendedorNotFoundException class

---
+++ Vendedor-service/src/main/java/com/example/vendedorservice/exception/VendedorNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.vendedorservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class VendedorNotFoundException extends RuntimeException{
+    public VendedorNotFoundException(String message) {
+        super(message);
+    }
+}
