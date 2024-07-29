0616f499276a3e4958c09451c5524ef4798f793b
Alejandro Casiva <alec667@gmail.com>
2024-07-28 22:38:52 -0300

    feature(Cliente-service/model.Cliente): add Cliente class

---
+++ Clientes-service/src/main/java/com/example/clientesservice/model/Cliente.java
@@ -0,0 +1,21 @@
+package com.example.clientesservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "cliente")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    private Integer clienteId;
+    private String clienteName;
+    private String descripcion;
+}
