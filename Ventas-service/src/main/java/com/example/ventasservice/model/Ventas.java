cf3142df9d5666611fcf3482ffbe3dccc2ff4612
Alejandro Casiva <alec667@gmail.com>
2024-07-30 21:08:48 -0300

    feature(Ventas-service/model.Ventas): add Ventas class

---
+++ Ventas-service/src/main/java/com/example/ventasservice/model/Ventas.java
@@ -0,0 +1,21 @@
+package com.example.ventasservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "ventas")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ventas {
+
+    @Id
+    private Integer ventaId;
+    private String descripcion;
+
+}
