8fe6f6afdbf76429c692ebd2349c4f6e5df76967
Alejandro Casiva <alec667@gmail.com>
2024-07-30 22:44:01 -0300

    feature(Ventas-service/service.VentasService): add VentasService and VentasServiceImpl classes

---
+++ Ventas-service/src/main/java/com/example/ventasservice/service/VentasService.java
@@ -0,0 +1,14 @@
+package com.example.ventasservice.service;
+
+import com.example.ventasservice.model.Ventas;
+
+import java.util.List;
+
+public interface VentasService {
+
+    Ventas getVenta(Integer ventaId);
+    List<Ventas> getAllVentas();
+    String createVenta(Ventas ventas);
+    Ventas updateVenta(Ventas ventas);
+    String deleteVenta(Integer ventaId);
+}
---
+++ Ventas-service/src/main/java/com/example/ventasservice/service/impl/VentasServiceImpl.java
@@ -0,0 +1,78 @@
+package com.example.ventasservice.service.impl;
+
+import com.example.ventasservice.dao.VentasDAO;
+import com.example.ventasservice.exception.VentaNotFoundException;
+import com.example.ventasservice.model.Ventas;
+import com.example.ventasservice.service.VentasService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VentasServiceImpl implements VentasService {
+
+    final
+    VentasDAO dao;
+
+    public VentasServiceImpl(VentasDAO dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public Ventas getVenta(Integer ventaId) {
+        Ventas venta = new Ventas();
+        Optional<Ventas> optional = dao.findById(ventaId);
+        if (optional.isPresent()) {
+            venta = optional.get();
+            return venta;
+        } else {
+            throw new VentaNotFoundException("Venta no encontrada");
+        }
+    }
+
+    @Override
+    public List<Ventas> getAllVentas() {
+        List<Ventas> ventasList = new ArrayList<>();
+        ventasList = dao.findAll();
+        return ventasList;
+    }
+
+    @Override
+    public String createVenta(Ventas ventas) {
+        try {
+            dao.save(ventas);
+            return "Venta creada";
+        } catch (Exception e) {
+            return e.getLocalizedMessage();
+        }
+    }
+
+    @Override
+    public Ventas updateVenta(Ventas ventas) {
+        Ventas updatedVenta = new Ventas();
+        Optional<Ventas> optional = dao.findById(ventas.getVentaId());
+        if (optional.isPresent()) {
+            updatedVenta = optional.get();
+            updatedVenta.setDescripcion(ventas.getDescripcion());
+            dao.save(updatedVenta);
+            return updatedVenta;
+        } else {
+            createVenta(ventas);
+            return ventas;
+        }
+
+    }
+
+    @Override
+    public String deleteVenta(Integer ventaId) {
+        Optional<Ventas> optional = dao.findById(ventaId);
+        if (optional.isPresent()) {
+            dao.deleteById(ventaId);
+            return "Venta Id: " + ventaId + " BORRADA.";
+        } else {
+            return "Venta Id: " + ventaId + " No Existe.";
+        }
+    }
+}
