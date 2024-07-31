fbaed9f4a572a722796587c3e8bff799650cad50
Alejandro Casiva <alec667@gmail.com>
2024-07-30 22:58:34 -0300

    feature(Ventas-service/controller.VentasController): add VentasController class

---
+++ Ventas-service/src/main/java/com/example/ventasservice/controller/VentasController.java
@@ -0,0 +1,43 @@
+package com.example.ventasservice.controller;
+
+import com.example.ventasservice.model.Ventas;
+import com.example.ventasservice.service.VentasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/ventas")
+public class VentasController {
+
+    @Autowired
+    VentasService ventasService;
+
+    @GetMapping(path = "{ventaId}", produces = "application/json")
+    public ResponseEntity<Ventas> getVenta(@PathVariable("ventaId") Integer ventaId) {
+        return new ResponseEntity<>(ventasService.getVenta(ventaId), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/all", produces = "application/json")
+    public ResponseEntity<List<Ventas>> getAlVentas() {
+        return new ResponseEntity<>(ventasService.getAllVentas(), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/create", consumes = "application/json")
+    public ResponseEntity<String> createVenta(@RequestBody Ventas venta) {
+        return new ResponseEntity<>(ventasService.createVenta(venta), HttpStatus.CREATED);
+    }
+
+    @PutMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Ventas> updateVenta(@RequestBody Ventas venta) {
+        return new ResponseEntity<>(ventasService.updateVenta(venta), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "{ventaId}")
+    public ResponseEntity<String> deleteVenta(@PathVariable("ventaId") Integer ventaId) {
+        return new ResponseEntity<>(ventasService.deleteVenta(ventaId), HttpStatus.OK);
+    }
+}
