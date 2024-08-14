ad38a927a2034f27d51cd616f9bcfd8ea441e010
Alejandro Casiva <alec667@gmail.com>
2024-08-14 20:15:15 -0300

    feature(api-gateway) add spring cloud API gateway

---
+++ api-gateway/src/test/java/com/example/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads(ApplicationContext context) {
+        assertThat(context).isNotNull();
+    }
+
+}
